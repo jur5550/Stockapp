064cd308386048fb95c5f3fcdf85ffaaa715ed73
Jur van Zonneveld <jurvanzonneveld@gmail.com>
2017-10-02 17:41:33 +0200

    Initial

---
+++ StockApp.java
@@ -0,0 +1,16 @@
+// the Client
+public class StockApp{
+    public static void main(String[] args){
+
+        // create concrete subject
+        StockGrabber stockGrabber = new StockGrabber();
+
+        // create and register concrete observer
+        StockObserver observer1 = new StockObserver(stockGrabber);
+
+        stockGrabber.setIBMPrice(197.00);
+        stockGrabber.setAAPLPrice(677.60);
+        stockGrabber.setGOOGPrice(676.40);
+    }
+}
+
